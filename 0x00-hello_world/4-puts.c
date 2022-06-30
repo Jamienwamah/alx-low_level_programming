fe238f48b98e7a677bb49ec4d85813f16741ae5e
oishdh <ikechukwuarinze614@gmail.com>
2022-06-30 12:50:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-int main(void) 
-{
-	/* Write a program that prints exactly
-	 * programming is like building a multi-lingual puzzle
-	 * followed by a new line
-	 */
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
-}
+1	#include <stdio.h>
+2
+3	/**
+4       * main - prints the size of various types
+5	* Return: 0 if exited properly, non-zero otherwise
+6       */
+7	int main(void)
+8	{
+9	puts("\"Programming is like building a multilingual puzzle");
+10	return (0);
+11	}
