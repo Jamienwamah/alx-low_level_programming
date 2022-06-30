9ed4582bd39aec4283b39f5edcb0721829301624
oishdh <ikechukwuarinze614@gmail.com>
2022-06-30 16:24:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 /**
  * main - prints the size of various types
- *
- * Returns: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
