46c6a4990d634fb19c26a332443fb42fc74d2950
oishdh <ikechukwuarinze614@gmail.com>
2022-06-30 15:48:25 -0700

    3-name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Returns 0 if excited properly, non-zero otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
