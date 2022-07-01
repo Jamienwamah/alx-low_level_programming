df9c94adc42e69f4787ad27a8e3467f93d5395de
oishdh <ikechukwuarinze614@gmail.com>
2022-07-01 11:37:41 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
  *
  *  *and print the last digit of the number stored in the variable n
  *
- *  *Return: Always 0 (success)
+ *  *Return: Always 0 (Success)
  *
  */
 
@@ -15,18 +15,15 @@ int main(void)
 
 {
 	int n;
-
-	        srand(time(0));
-	
+	     srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 		printf("%d is positive\n", n);
-	if (n == 0)
+        if (n == 0)
 		printf("%d is zero\n", n);
 	if (n < 0)
 	{
-		printf("%d is negative\n", n);
+      		printf("%d is negative\n", n);
 	}
 
 	return (0);
