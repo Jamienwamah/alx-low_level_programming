6c207de272978edb9a37cab70631a3361815a890
oishdh <ikechukwuarinze614@gmail.com>
2022-07-01 15:57:16 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,28 +3,28 @@
 #include <stdio.h>
 
 /**
- *  *main -> assign a random numer to the variable n each time it is executed
+ **main -> assign a random numer to the variable n each time it is executed
  *
- *  *and print the last digit of the number stored in the variable n
+ **and print the last digit of the number stored in the variable n
  *
- *  *Return: Always 0 (Success)
+ **Return: Always 0 (Success)
  *
  */
 
 int main(void)
-
 {
-	int n;
-	     srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-        if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-	{
-      		printf("%d is negative\n", n);
-	}
+int n;
 
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%d is positive\n", n);
+if (n == 0)
+	printf("%d is zero\n", n);
+if (n < 0)
+{
+      	printf("%d is negative\n", n);
+}
+
+return (0);
 }
