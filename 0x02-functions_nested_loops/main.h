e7f2a2cedd337207f50d0cf5c582979bbd1324f9
oishdh <ikechukwuarinze614@gmail.com>
2022-07-05 09:40:28 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet(void);
+int -islower(int c);
+int -isalpha(int c);
+int print_sign(int n);
+int _abs(int r);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_times_table(int a);
+void putformat(int);
