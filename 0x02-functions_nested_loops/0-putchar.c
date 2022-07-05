daa71dc16020fb223008826028612f8f75b82060
oishdh <ikechukwuarinze614@gmail.com>
2022-07-05 10:40:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- **main - check description
+ *main - Entry point
  *
- **Description: It prints _putchar followed by a new line
- *
- **Return: Always 0 (Success)
- *
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char nord(8) = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(word[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
