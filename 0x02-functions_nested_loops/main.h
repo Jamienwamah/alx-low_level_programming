a08ebb93e5f5a36e012ebd1806f03145c1ba1f70
oishdh <ikechukwuarinze614@gmail.com>
2022-07-05 11:18:10 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 void print_alphabet(void);
-/*void print_alphabet(void);
-int -islower(int c);
+void print_alphabet(void);
+/*int -islower(int c);
 int -isalpha(int c);
 int print_sign(int n);
 int _abs(int r);
