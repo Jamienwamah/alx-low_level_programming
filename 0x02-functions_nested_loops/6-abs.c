a345448330bea74685b185dbca39db7a1688dc07
oishdh <ikechukwuarinze614@gmail.com>
2022-07-05 15:19:39 -0700

    6-main.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs-prints the absolute value of an integer
+ *@i: number targeted
+ *Return: Always 0 (Success)
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	return (i);
+	else if (i < 0)
+	return (-1);
+	else
+	return (0);
+}
