531dfab05e6fbe86e17baf6a29b3602465a46137
oishdh <ikechukwuarinze614@gmail.com>
2022-07-06 02:50:50 -0700

    10-main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _abs(int r);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-/*int add(int a, int b);
-void print_times_table(int a);
+int add(int a, int b);
+/*void print_times_table(int a);
 void putformat(int);*/
 
