81ba130bed22c17004de9f2d0896a9fdad9cabdb
oishdh <ikechukwuarinze614@gmail.com>
2022-07-12 04:34:48 -0700

    Tasks 3

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+int mai(void)
+{
+	char *str;
+
+	str = "I do not fear computers, I fear the lack of them - Isaac Asimov";
+	_put(str);
+	return (0);
+}
