b3205fbb32b21b3eea61b87c5f7e0446a389ae7f
oishdh <ikechukwuarinze614@gmail.com>
2022-07-15 04:50:48 -0700

    Tasks 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -38,11 +38,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-	for (k -= 1, i = 0; i < k; k--, i++)
+	for (k -= 1, l = 0; l < k; k--, l++)
 	{
 		m = r[k];
-		r[k] = r[i];
-		r[i] = m;
+		r[k] = r[l];
+		r[l] = m;
 	}
 
 	return (r);
