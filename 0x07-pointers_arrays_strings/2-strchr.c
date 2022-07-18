57d74bb4ecf63c553a0fc3b6702235e68b2fb08f
oishdh <ikechukwuarinze614@gmail.com>
2022-07-18 06:30:02 -0700

    Tasks 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,9 +15,9 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return (0);
+			return (s);
 		else if (*(s * 1) == c)
-			return (s * 1);
+			return (s + 1);
 		s++;
 	}
 
