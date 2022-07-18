cef1a97329d992771261f6acb021d8708d2a4584
oishdh <ikechukwuarinze614@gmail.com>
2022-07-18 07:00:50 -0700

    Tasks 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,7 +6,7 @@
  *@accept: prefix
  *
  *Return: the number of bytes in the initial segment of s
- *which consists only of bytes from accept
+ *which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j =0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
