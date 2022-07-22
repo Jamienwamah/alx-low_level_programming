2eb33c2b8766a3b2969cf9707cc5c99c27347d29
oishbdh <ikechukwuarinze614@gmail.com>
2022-07-22 10:46:23 -0700

    Tasks 0

---
+++ 0x09-static_libraries/codes/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *_isdigit - checks whether a a character is a digit
+  *@c : input character
+  *Return: 1 if the character is digit
+**/
+
+int _isdigit(int c)
+{
+	/*check whether c is a digit*/
+}
---
+++ 0x09-static_libraries/codes/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *_atoi - function that convert alphabet to an
+ integer
+  *@s : input alphabet
+  *Return: integer equivalent, not sure
+**/
+
+int _atoi(char *s)
+{
+	/*perform conversation here*/
+}
