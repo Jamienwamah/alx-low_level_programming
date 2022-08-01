3245bb56d41b37bb85e504472a58fc69d59da5ec
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-01 08:08:44 -0700

    Tasks 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *n, *o;
 
 	doge = malloc(sizeof(struct dog));
-	if (dog == NULL)
+	if (doge == NULL)
 		return (NULL);
 
 	for (i = 0; name[i] != '\0'; i++)
