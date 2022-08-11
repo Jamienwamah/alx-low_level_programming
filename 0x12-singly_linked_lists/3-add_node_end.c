9495b0e383dd2872e07f0c28676b2839457bb276
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-11 11:22:47 -0700

    Tasks 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,7 +3,7 @@
 /**
  *add_node_end - adds a new node at the end of a list_t list.
  *@str: string to be duplicated
- *
+ *@head: head of singly linked
  *Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
