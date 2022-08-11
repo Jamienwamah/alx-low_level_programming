e6c13b58df26f52aaa55d55a4571eb9ecdb267e5
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-11 09:59:15 -0700

    Tasks 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,53 +5,23 @@
 /**
  *print_list - prints all the elements of a list_t list.
  *If str is NULL, print [0] (nil)
- *@format: the format
+ *@h: pointer to the list_t list to print
  *@str: the string
  *Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	list_t *head;
-	list_t *new;
-	list_t hello = {"World", 5, NULL};
-	size_t n;
-	typedef struct Element Element;
-	
-	struct Element
-	{
-		int number;
-		Element *next;
-	};
-	
-	Element *addnewN(int number)
-	{
-		Element *newN = (Element*)malloc(sizeof(Element));
-		
-		newN->number = number;
-		newN->next = NULL;
-		
-		return newN;
-	}
-	
-	Element *add_on_beginning(Element *head, Element *newN)
-	{
-		newN->next = head;
-		
-		return head;
-	}
-}
+	size_t s = 0;
 
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
