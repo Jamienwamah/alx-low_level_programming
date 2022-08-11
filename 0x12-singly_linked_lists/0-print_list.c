c5627fe634fc2d89343c6406f210ce61a03299ae
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-11 10:33:30 -0700

    Tasks 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,6 @@
  *print_list - prints all the elements of a list_t list.
  *If str is NULL, print [0] (nil)
  *@h: pointer to the list_t list to print
- *@str: the string
  *Return: the number of nodes
  */
 size_t print_list(const list_t *h)
