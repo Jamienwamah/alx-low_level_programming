b07360eae75f2a33357a37df371c7b90b3ae6e67
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-16 12:36:50 -0700

    Tasks 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,124 +1,47 @@
 #include "lists.h"
 
-
 /**
-* free_listp - frees a linked list
-* @head: head of a list.
-* Return: no return.
-*/
-
-void free_listp(listp_t **head)
-
+ * print_listint_safe - prints a list even with loop
+ * @head: pointer to head
+ *
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
 {
+	int count = 0;
+	const listint_t *visited[1024];
 
-	listp_t *temp;
-
-	listp_t *curr;
-
-
-
-	if (head != NULL)
-
+	while (head)
 	{
-
-		curr = *head;
-
-		while ((temp = curr) != NULL)
-
+		if (is_visited(head, visited, count))
 		{
-
-			curr = curr->next;
-
-			free(temp);
-
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
 		}
-
-		*head = NULL;
-
+		visited[count++] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
+	return (count);
 }
 
-
-
 /**
-* print_listint_safe - prints a linked list.
-* @head: head of a list.
-* Return: number of nodes in the list.
-*/
-
-size_t print_listint_safe(const listint_t *head)
-
+ * is_visited - check if a node is visited
+ * @node: pointer to node
+ * @visited: list of visited
+ * @count: length of visited
+ *
+ * Return: 1 if is visited and 0 otherwise
+ */
+int is_visited(const listint_t *node, const listint_t **visited, int count)
 {
+	int i = 0;
 
-	size_t nnodes = 0;
-
-	listp_t *hptr, *new, *add;
-
-
-
-	hptr = NULL;
-
-	while (head != NULL)
-
+	while (i < count)
 	{
-
-		new = malloc(sizeof(listp_t));
-
-
-
-		if (new == NULL)
-
-			exit(98);
-
-
-
-		new->p = (void *)head;
-
-		new->next = hptr;
-
-		hptr = new;
-
-
-
-		add = hptr;
-
-
-
-		while (add->next != NULL)
-
-		{
-
-			add = add->next;
-
-			if (head == add->p)
-
-			{
-
-				printf("-> [%p] %d\n", (void *)head, head->n);
-
-				free_listp(&hptr);
-
-				return (nnodes);
-
-			}
-
-		}
-
-
-
-		printf("[%p] %d\n", (void *)head, head->n);
-
-		head = head->next;
-
-		nnodes++;
-
+		if (node == visited[i])
+			return (1);
+		i++;
 	}
-
-
-
-	free_listp(&hptr);
-
-	return (nnodes);
-
+	return (0);
 }
