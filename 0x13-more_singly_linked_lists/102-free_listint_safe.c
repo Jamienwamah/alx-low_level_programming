93fc6ed2650e8fc9dae544332d3ab2c5ba6af611
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-16 12:33:53 -0700

    Tasks 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,131 +1,52 @@
 #include "lists.h"
 
+int is_visited2(listint_t *node, listint_t **visited, int count);
 
 /**
-
-* free_listp2 - frees a linked list
-* @head: head of a list.
-* Return: no return.
-*/
-
-void free_listp2(listp_t **head)
-
+ * free_listint_safe - prints a list even with loop
+ * @head: pointer to head
+ *
+ * Return: number of nodes
+ */
+size_t free_listint_safe(listint_t **head)
 {
+	listint_t *tmp, *visited[1024];
+	int count = 0;
 
-	listp_t *temp;
-
-	listp_t *curr;
-
-
-
-	if (head != NULL)
-
+	if (!head)
+		return (-1);
+	while (*head)
 	{
-
-		curr = *head;
-
-		while ((temp = curr) != NULL)
-
+		if (is_visited2(*head, visited, count))
 		{
-
-			curr = curr->next;
-
-			free(temp);
-
+			*head = NULL;
+			break;
 		}
-
-		*head = NULL;
-
+		visited[count++] = *head;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
-
+	return (count);
 }
 
-
-
 /**
-* free_listint_safe - frees a linked list.
-* @h: head of a list.
-* Return: size of the list that was freed.
-*/
-
-size_t free_listint_safe(listint_t **h)
-
+ * is_visited2 - check if a node is visited
+ * @node: pointer to node
+ * @visited: list of visited
+ * @count: length of visited
+ *
+ * Return: 1 if is visited and 0 otherwise
+ */
+int is_visited2(listint_t *node, listint_t **visited, int count)
 {
+	int i = 0;
 
-	size_t nnodes = 0;
-
-	listp_t *hptr, *new, *add;
-
-	listint_t *curr;
-
-
-
-	hptr = NULL;
-
-	while (*h != NULL)
-
+	while (i < count)
 	{
-
-		new = malloc(sizeof(listp_t));
-
-
-
-		if (new == NULL)
-
-			exit(98);
-
-
-
-		new->p = (void *)*h;
-
-		new->next = hptr;
-
-		hptr = new;
-
-
-
-		add = hptr;
-
-
-
-		while (add->next != NULL)
-
-		{
-
-			add = add->next;
-
-			if (*h == add->p)
-
-			{
-
-				*h = NULL;
-
-				free_listp2(&hptr);
-
-				return (nnodes);
-
-			}
-
-		}
-
-
-
-		curr = *h;
-
-		*h = (*h)->next;
-
-		free(curr);
-
-		nnodes++;
-
+		if (node == visited[i])
+			return (1);
+		i++;
 	}
-
-
-
-	*h = NULL;
-
-	free_listp2(&hptr);
-
-	return (nnodes);
-
+	return (0);
 }
