b056e93916af2c0086777ef77b47dde8bd6ba1f0
oishbdh <ikechukwuarinze614@gmail.com>
2022-08-18 08:01:41 -0700

    Tasks 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i = 1, len = 0;
-	
-	while (len++ < index)
-		i = i << 1;
-	if (i > n)
-		return (-1);
-	return(n & i ? 1 : 0);
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
+	return (-1);
+	*n |= (1 << index);
+	return (1);
 }
