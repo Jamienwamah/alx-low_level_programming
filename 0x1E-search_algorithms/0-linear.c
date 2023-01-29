7419bd4f64ff8fe4ec756988f32a73d957e40b6b
“oishbdh” <ikechukwuarinze614@gmail.com>
2023-01-29 15:07:44 -0800

    updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,17 +1,17 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches an array linearly
- * @array: pointer to array
- * @size: size of array
- * @value: key to search
+ *linear_search - searches an array linearly
+ *@array: pointer to array
+ *@size: size of array
+ *@value: key to search
  *
- * Return: index of value or -1
+ *Return: index of value or -1
  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
-
+	
 	if (!array)
 		return (-1);
 	for (; i < size; i++)
